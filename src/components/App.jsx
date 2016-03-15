9e79cb520cda7419519408299e9981bd6bd929be
Athman Gude <athmangude@gmail.com>
2016-03-15 09:22:32 +0300

    fixed issues in setting up material-ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import MessageList from './MessageList.jsx';
 
+import AppBar from 'material-ui/lib/app-bar';
+
+import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
+import MyCustomTheme from './../MaterialTheme.js';
+
 class App extends Component {
     constructor(){
         super();
     }
 
+    //the key passed through context must be called "muiTheme"
+    static childContextTypes = {
+        muiTheme: React.PropTypes.object,
+    }
+
+    getChildContext() {
+        return {
+            muiTheme: getMuiTheme(MyCustomTheme),
+        };
+    }
+
+
     render() {
         return (
-            <MessageList />
+            <div>
+                <AppBar title="My AppBar" />
+                <MessageList />
+            </div>
         );
     }
 }
