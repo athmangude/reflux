04610029d015f7cc8ce7328285debe7d3e0ed647
Athman Gude <athmangude@gmail.com>
2016-03-16 10:13:40 +0300

    sending messages to Firebase

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import mui from 'material-ui';
+import trim from 'trim';
+import Firebase from 'firebase';
 
 var { Card } = mui;
 
 class MessageBox extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            message: ''
+        };
+
+        this.firebaseRef = new Firebase('https://refluxy.firebaseio.com/messages');
+    }
+
+    onChange(event) {
+        this.setState({
+            message: event.target.value
+        })
+    }
+
+    onKeyUp(event) {
+        if (event.keyCode === 13 && trim(event.target.value) !== '') {
+            event.preventDefault();
+
+            this.firebaseRef.push({
+                message: this.state.message
+            });
+
+            this.setState({
+                message: ''
+            });
+        }
     }
 
     render() {
@@ -15,17 +43,21 @@ class MessageBox extends React.Component {
                 margin: '30px auto',
                 padding: 20
             }}>
-                <textarea style={{
-                    width: '100%',
-                    borderColor: '#D0D0D0',
-                    resize: 'none',
-                    borderRadius: 3,
-                    minHeight: 50,
-                    color: '#555',
-                    fontSize: 14,
-                    outline: 'auto 0px',
-                    padding: 5
-                }}></textarea>
+                <textarea
+                    value = {this.state.message}
+                    onChange = {this.onChange.bind(this)}
+                    onKeyUp = {this.onKeyUp.bind(this)}
+                    style={{
+                        width: '100%',
+                        borderColor: '#D0D0D0',
+                        resize: 'none',
+                        borderRadius: 3,
+                        minHeight: 50,
+                        color: '#555',
+                        fontSize: 14,
+                        outline: 'auto 0px',
+                        padding: 5
+                    }}></textarea>
             </Card>
         );
     }
