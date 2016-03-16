dc6db6182af4f8b77781d950885be5b4d2e9dabe
Athman Gude <athmangude@gmail.com>
2016-03-16 10:14:05 +0300

    handling adding and deleting of events

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -17,28 +17,33 @@ class MessageList extends React.Component {
         };
 
         this.firebaseRef = new Firebase('https://refluxy.firebaseio.com/messages');
-        this.firebaseRef.once('value', (dataSnapshot) => {
-            var messagesVal = dataSnapshot.val();
 
-            var messages = _(messagesVal)
-                .keys()
-                .map((messageKey) => {
-                    var cloned = _.clone(messagesVal[messageKey]);
-                    cloned.key = parseInt(messageKey);
-                    return cloned;
-                })
-                .value();
+        // listening for added events
+        this.firebaseRef.on('child_added', (message) => {
+            if (this.state.messages[message.key()]) {
+                return;
+            }
+
+            this.state.messages[message.key()] = message.val();
 
             this.setState({
-                messages: messages
+                messages: this.state.messages
             });
-
-            console.log(this.state.messages);
         });
+
+        // listening for delete events
+        this.firebaseRef.on('child_removed', (message) => {
+            delete this.state.messages[message.key()];
+
+            this.setState({
+                messages: this.state.messages
+            });
+        });
+
     }
 
     render() {
-        var messageNodes = this.state.messages.map((message) => {
+        var messageNodes = _.values(this.state.messages).map((message) => {
             return (
                 <Message message={message.message} />
             );
